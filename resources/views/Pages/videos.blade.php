95cd106548a539dc8205ce17888b213cce23ee49
James Titus Wilson <jwilsonkla@gmail.com>
2019-10-12 14:20:27 +0000

    Added videos page

---
+++ resources/views/Pages/videos.blade.php
@@ -0,0 +1,368 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="educational-responsive-Richard-Nanah-Gaye-Acamedy">
+    <meta name="author" content="webThemez.com">
+    <title>Richard Nanah Gaye Acamedy</title>
+    <link rel="favicon" href="dist/images/favicon.png">
+    <link rel="stylesheet" media="screen" href="dist/http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="dist/css/bootstrap-theme.css" media="screen"> 
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel='stylesheet' id='camera-css'  href='dist/css/camera.css' type='text/css' media='all'> 
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- Button for smallest screens -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="dist/images/logo - copy.png" alt="Techro HTML5 template"></a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right mainNav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="{{route('about.show')}}">About</a></li>
+                    <li><a href="{{route('courses.show')}}">Courses</a></li>
+                    <li><a href="{{route('events.show')}}">Events</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('events.show')}}">Activities</a></li>
+                            <li><a href="{{route('photos.show')}}">Photos</a></li>
+                            <li><a href="{{route('videos.show')}}">Videos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{route('contact.add')}}">Contact</a></li>
+
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+    <!-- /.navbar -->
+
+<header id="head" class="secondary">
+            <div class="container">
+                    <h1>Events Videos</h1>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing eliras scele!</p>
+                </div>
+    </header>
+
+
+    <!-- container -->
+    <section class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <section id="portfolio" class="page-section section appear clearfix">
+                    <br />
+                    <br />
+                    <p>
+                        At lorem Ipsum available, but the majority have suffered alteration in some form by injected huffered altehe majority have suffered alteration in some form by injected huffered alteration in some form by injected humour.uffered alteration in some form by injected h
+                    <br />
+                        <br />
+                    </p>
+
+
+                    <div class="row">
+                        <nav id="filter" class="col-md-12 text-center">
+                            <ul>
+                                <li><a href="#" class="current btn-theme btn-small" data-filter="*">All</a></li>
+                                <li><a href="#" class="btn-theme btn-small" data-filter=".webdesign">Galler Day</a></li>
+                                <li><a href="#" class="btn-theme btn-small" data-filter=".photography">Our Best Student</a></li>
+                                <li><a href="#" class="btn-theme btn-small" data-filter=".print">Teacher Day</a></li>
+                            </ul>
+                        </nav>
+                        <div class="col-md-12">
+                            <div class="row">
+                                <div class="portfolio-items isotopeWrapper clearfix" id="3">
+
+                                    <article class="col-sm-4 isotopeItem webdesign">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img1.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img1.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem photography">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img2.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img2.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+
+                                    <article class="col-sm-4 isotopeItem photography">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img3.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img3.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem print">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img4.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img4.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem photography">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img5.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img5.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem webdesign">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img6.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img6.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem print">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img7.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img7.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem photography">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img8.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img8.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+
+                                    <article class="col-sm-4 isotopeItem print">
+                                        <div class="portfolio-item">
+                                            <img src="dist/images/portfolio/img9.jpg" alt="" />
+                                            <div class="portfolio-desc align-center">
+                                                <div class="folio-info">
+                                                    <a href="dist/images/portfolio/img9.jpg" class="fancybox">
+                                                        <h5>Project Title</h5>
+                                                        <i class="fa fa-link fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                </div>
+
+                            </div>
+
+
+                        </div>
+                    </div>
+
+                </section>
+            </div>
+        </div>
+
+    </section>
+    <!-- /container -->
+
+
+<footer id="footer">
+ 
+        <div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology/Computer
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business Skills
+              </a>
+            </li>
+            <li><a href="#">
+               List of Home Economic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="{{route('courses.show')}}">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+                <div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Enroll now... By visiting our campous.</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kabbah Township, Montserrado Cunty, Liberia<br>
+            <i class="fa fa-phone"></i>+123 (0)886 521 451 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+            <div class="social text-center">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+
+            <div class="clear"></div>
+            <!--CLEAR FLOATS-->
+        </div>
+        <div class="footer2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="simplenav">
+                                <a href="/">Home</a> | 
+                                <a href="{{route('about.show')}}">About</a> |
+                                <a href="{{route('courses.show')}}">Courses</a> |
+                                <a href="{{route('events.show')}}">Events</a> |
+                                <a href="{{route('contact.add')}}">Contact</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="text-right">
+                                Copyright &copy; 2019. Template by <a rel="develop">Bolex88</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /row of panels -->
+            </div>
+        </div>
+    </footer>
+
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="dist/js/custom.js"></script>
+</body>
+</html>
