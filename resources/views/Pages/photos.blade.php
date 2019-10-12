efb448c3924a9e9b8472c2af69ffd346f3b81fe9
James Titus Wilson <jwilsonkla@gmail.com>
2019-10-12 14:19:46 +0000

    added photos page

---
+++ resources/views/Pages/photos.blade.php
@@ -0,0 +1,287 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Richard Nanah Gaye Acamedy</title>
+    <!-- Loading third party fonts -->
+	<link href="http://fonts.googleapis.com/css?family=Arvo:400,700|" rel="stylesheet" type="text/css">
+    <link href="fonts/font-awesome.min.css" rel="stylesheet" type="text/css">
+		<!-- Loading main css file -->
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="dist/css/bootstrap-theme.css" media="screen"> 
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel='stylesheet' id='camera-css'  href='dist/css/camera.css' type='text/css' media='all'> 
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- Button for smallest screens -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="dist/images/logo - copy.png" alt="Techro HTML5 template"></a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right mainNav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="{{route('about.show')}}">About</a></li>
+                    <li><a href="{{route('courses.show')}}">Courses</a></li>
+                    <li><a href="{{route('events.show')}}">Events</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('events.show')}}">Activities</a></li>
+                            <li><a href="{{route('photos.show')}}">Photos</a></li>
+                            <li><a href="{{route('videos.show')}}">Videos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{route('contact.add')}}">Contact</a></li>
+
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+    <!-- /.navbar -->
+
+    </head>
+    <body>
+				<div class="page-title">
+					<div class="container">
+						<h2>Our Student are our pride</h2>
+					</div>
+				</div>
+			</header>
+		</div>
+
+		<main class="main-content">
+			<div class="fullwidth-block inner-content">
+				<div class="container">
+					<div class="fullwidth-content">
+						<p class="leading">Every student matters at RINEGA.</p>
+						<p>We support our student in every step of the way, because we want them to be sucessful not only during their acadmic learning program, but also when they leave us for feature pathway.</p>
+
+						<div class="pagination">
+							<a href="#" class="page-numbers"><i class="fa fa-angle-left"></i></a>
+							<a href="#" class="page-numbers">1</a>
+							<span class="page-numbers current">2</span>
+							<a href="#" class="page-numbers">3</a>
+							<a href="#" class="page-numbers">4</a>
+							<a href="#" class="page-numbers">5</a>
+							<a href="#" class="page-numbers"><i class="fa fa-angle-right"></i></a>
+						</div>
+
+						<ul class="students-grid">
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student2.jpg" alt="Student 1"></figure>
+								<h3 class="student-name"><a href="#">Monica Kingston</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student11.jpg" alt="Student 2"></figure>
+								<h3 class="student-name"><a href="#">Carol Blumkin</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/Student13.jpg" alt="Student 3"></figure>
+								<h3 class="student-name"><a href="#">Martina Schulz</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student15.jpg" alt="Student 4"></figure>
+								<h3 class="student-name"><a href="#">Christina Stewart</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student16.jpg" alt="Student 5"></figure>
+								<h3 class="student-name"><a href="#">Arthur Smith</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student7.jpg" alt="Student 6"></figure>
+								<h3 class="student-name"><a href="#">Steven johnson</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student8.jpg" alt="Student 7"></figure>
+								<h3 class="student-name"><a href="#">Sarah James</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+							<li class="student">
+								<figure class="avatar"><img src="dist/images/Students/student9.jpg" alt="Student 8"></figure>
+								<h3 class="student-name"><a href="#">George Harrington</a></h3>
+								<span class="average">Average: 4,9</span>
+							</li>
+						</ul>
+
+						<div class="pagination">
+							<a href="#" class="page-numbers"><i class="fa fa-angle-left"></i></a>
+							<a href="#" class="page-numbers">1</a>
+							<span class="page-numbers current">2</span>
+							<a href="#" class="page-numbers">3</a>
+							<a href="#" class="page-numbers">4</a>
+							<a href="#" class="page-numbers">5</a>
+							<a href="#" class="page-numbers"><i class="fa fa-angle-right"></i></a>
+						</div>
+					</div>
+				</div>
+			</div> <!-- .fullwidth-block -->
+		</main>
+
+<footer id="footer">
+ 
+		<div class="container">
+		   <div class="row">
+		  <div class="footerbottom">
+		    <div class="col-md-3 col-sm-6">
+		      <div class="footerwidget">
+		        <h4>
+		          Course Categories
+		        </h4>
+		        <div class="menu-course">
+		          <ul class="menu">
+		            <li><a href="#">
+		                List of Technology/Computer
+		              </a>
+		            </li>
+		            <li><a href="#">
+		                List of Business Skills
+		              </a>
+		            </li>
+		            <li><a href="#">
+		               List of Home Economic
+		              </a>
+		            </li>
+		          </ul>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col-md-3 col-sm-6">
+		      <div class="footerwidget">
+		        <h4>
+		          Products Categories
+		        </h4>
+		        <div class="menu-course">
+		          <ul class="menu">
+		            <li> <a href="#">
+		                Individual Plans  </a>
+		            </li>
+		            <li><a href="#">
+		                Business Plans
+		              </a>
+		            </li>
+		            <li><a href="#">
+		                Free Trial
+		              </a>
+		            </li>
+		            <li><a href="#">
+		                Academic
+		              </a>
+		            </li>
+		          </ul>
+		        </div>
+		      </div>
+		    </div>
+		    <div class="col-md-3 col-sm-6">
+		      <div class="footerwidget">
+		        <h4>
+		          Browse by Categories
+		        </h4>
+		        <div class="menu-course">
+		          <ul class="menu">
+		            <li><a href="{{route('courses.show')}}">
+		                All Courses
+		              </a>
+		            </li>
+		            <li> <a href="#">
+		                All Instructors
+		              </a>
+		            </li>
+		            <li><a href="#">
+		                All Members
+		              </a>
+		            </li>
+		            <li>
+		              <a href="#">
+		                All Groups
+		              </a>
+		            </li>
+		          </ul>
+		        </div>
+		      </div>
+		    </div>
+            <div class="col-md-3 col-sm-6"> 
+            	<div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Enroll now... By visiting our campous.</p>
+		            <div class="contact-info"> 
+		            <i class="fa fa-map-marker"></i> Kabbah Township, Montserrado Cunty, Liberia<br>
+		            <i class="fa fa-phone"></i>+123 (0)886 521 451 <br>
+		             <i class="fa fa-envelope-o"></i> youremail@email.com
+		         </div> 
+            </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="/">Home</a> | 
+								<a href="{{route('about.show')}}">About</a> |
+								<a href="{{route('courses.show')}}">Courses</a> |
+								<a href="{{route('events.show')}}">Events</a> |
+								<a href="{{route('contact.add')}}">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2019. Template by <a rel="develop">Bolex88</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+	<script src="dist/js/jquery-1.11.1.min.js"></script>
+	<script src="dist/js/plugins.js"></script>
+	<script src="dist/js/app.js"></script>
+</body>
+</html>
