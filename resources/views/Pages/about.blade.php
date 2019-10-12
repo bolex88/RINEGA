cee9d6b81e56735ce363cd7b62f2a7af7fe7ace5
James Titus Wilson <jwilsonkla@gmail.com>
2019-10-12 14:17:52 +0000

    added the about page

---
+++ resources/views/Pages/about.blade.php
@@ -0,0 +1,344 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="educational-responsive-Richard-Nanah-Gaye-Acamedy">
+    <meta name="author" content="webThemez.com">
+    <title>Richard Nanah Gaye Acamedy</title>
+    <link rel="favicon" href="dist/images/favicon.png">
+    <link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css">
+    <!-- Custom styles for our template -->
+    <link rel="stylesheet" href="dist/css/bootstrap-theme.css" media="screen">
+    <link rel="stylesheet" href="dist/css/style.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- Button for smallest screens -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="dist/images/logo - copy.png" alt="Techro HTML5 template"></a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right mainNav">
+                    <li><a href="/">Home</a></li>
+                    <li class="active"><a href="{{route('about.show')}}">About</a></li>
+                    <li><a href="{{route('courses.show')}}">Courses</a></li>
+                    <li><a href="{{route('events.show')}}">Events</a></li>
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('events.show')}}">Activities</a></li>
+                            <li><a href="{{route('photos.show')}}">Photos</a></li>
+                            <li><a href="{{route('videos.show')}}">Videos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{route('contact.add')}}">Contact</a></li>
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+    <!-- /.navbar -->
+
+    <header id="head" class="secondary">
+            <div class="container">
+                    <h1>About Us</h1>
+                    <p>At Richard Nanah Gaye Acamedy our "Learning has no Limits", and this is firmly embedded in our learning community!</p>
+                </div>
+    </header>
+
+
+    <!-- container -->
+    <section class="container">
+        <div class="row">
+            <!-- main content -->
+            <section class="col-sm-8 maincontent">
+                <h3>About Us</h3>
+                <p>
+                    <img src="dist/images/Family.jpg" alt="" class="img-rounded pull-right" width="300">
+                    Our focus is to support and develop enthusiastic students, and instal in them a love of learning so as to help them reach their potential, both socially and academically. As a learning community, we pride ourselves in the development of the whole child, values and social skills are an importan part of the school curriumlum and we are focused on providing an inclusive education for all.</p><p>We are committed to providing a high qualityteaching and learning program that gives all of our students the opportunity to achieve success. With high expectations, we work together with the wider school community to prepare our students to become respected and contributing members of society.
+                </p>
+                <h3>Our Vision</h3>
+                <p>Our vision is to inspire you to engage in learning and to support you to achieve your  educational goals. Every student matters at RINEGA. You will be supported every step of the way because we want you to be sucessful not only during your learning program, but also when you leave us on your feature pathway.</p>
+                <h3>Our Achievements</h3>
+                <strong>2014</strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam beatae soluta accusantium iusto nihil nesciunt unde veniam magnam repudiandae sapiente. consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam!</p>
+                <strong>2013</strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam beatae soluta accusantium iusto nihil nesciunt unde veniam magnam repudiandae sapiente. consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam!</p>
+                <strong>2012</strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam beatae soluta accusantium iusto nihil nesciunt unde veniam magnam repudiandae sapiente. consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam!</p>
+                <strong>2011</strong>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet, consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam beatae soluta accusantium iusto nihil nesciunt unde veniam magnam repudiandae sapiente. consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam laudantium voluptates placeat consectetur quam aliquam!</p>
+            </section>
+            <!-- /main -->
+
+            <!-- Sidebar -->
+            <aside class="col-sm-4 sidebar sidebar-right">
+
+                <div class="panel">
+                    <h4>Latest News</h4>
+                    <ul class="list-unstyled list-spaces">
+                        <li><a href="">Computer Technology</a><br>
+                            <span class="small text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi, laborum.</span></li>
+                        <li><a href="">Tylering</a><br>
+                            <span class="small text-muted">Consequuntur eius repellendus eos aliquid molestiae ea laborum ex quibusdam</span></li>
+                        <li><a href="">Home Economics</a><br>
+                            <span class="small text-muted">Eveniet, consequuntur eius repellendus eos aliquid molestiae ea</span></li>
+                        <li><a href="">Office Management</a><br>
+                            <span class="small text-muted">Sed, mollitia earum debitis est itaque esse reiciendis amet cupiditate.</span></li>
+                    </ul>
+                </div>
+
+            </aside>
+            <!-- /Sidebar -->
+
+        </div>
+    </section>
+    <!-- /container -->
+    <section class="team-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Our Team</h3>
+                    <p>A dedicated and enthusiastic professional staff. A strong supportive partners, and a well-resourced learning environment.</p>
+                    <br />
+                </div>
+            </div>
+            <div class="row">
+
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="dist/images/photo-1.jpg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>John Doe</h4>
+                        <!-- Designation -->
+                        <span class="pos">CEO</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="dist/images/photo-2.jpg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Larry Doe</h4>
+                        <!-- Designation -->
+                        <span class="pos">Director</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="dist/images/photo-3.jpg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Ranith Kays</h4>
+                        <!-- Designation -->
+                        <span class="pos">Manager</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-6">
+                    <!-- Team Member -->
+                    <div class="team-member pDark">
+                        <!-- Image Hover Block -->
+                        <div class="member-img">
+                            <!-- Image  -->
+                            <img class="img-responsive" src="dist/images/photo-4.jpg" alt="">
+                        </div>
+                        <!-- Member Details -->
+                        <h4>Joan Ray</h4>
+                        <!-- Designation -->
+                        <span class="pos">Creative</span>
+                        <div class="team-socials">
+                            <a href="#"><i class="fa fa-facebook"></i></a>
+                            <a href="#"><i class="fa fa-google-plus"></i></a>
+                            <a href="#"><i class="fa fa-twitter"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  <footer id="footer">
+ 
+        <div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology/Computer
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business Skills
+              </a>
+            </li>
+            <li><a href="#">
+               List of Home Economic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="{{route('courses.show')}}">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+                <div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Enroll now... By visiting our campous.</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kabbah Township, Montserrado Cunty, Liberia<br>
+            <i class="fa fa-phone"></i>+123 (0)886 521 451 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+            <div class="social text-center">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+
+            <div class="clear"></div>
+            <!--CLEAR FLOATS-->
+        </div>
+        <div class="footer2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="simplenav">
+                                <a href="/">Home</a> | 
+                                <a href="{{route('about.show')}}">About</a> |
+                                <a href="{{route('courses.show')}}">Courses</a> |
+                                <a href="{{route('events.show')}}">Events</a> |
+                                <a href="{{route('contact.add')}}">Contact</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="text-right">
+                                Copyright &copy; 2019. Template by <a rel="develop">Bolex88</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /row of panels -->
+            </div>
+        </div>
+    </footer>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <script src="dist/js/custom.js"></script>
+</body>
+</html>
