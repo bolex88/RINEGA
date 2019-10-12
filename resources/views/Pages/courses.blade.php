ce564b50c6ecd79a29dab85868bbc0643ed1c2b1
James Titus Wilson <jwilsonkla@gmail.com>
2019-10-12 14:18:47 +0000

    added courses page

---
+++ resources/views/Pages/courses.blade.php
@@ -0,0 +1,331 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="description" content="educational-responsive-Richard-Nanah-Gaye-Acamedy">
+	<meta name="author" content="webThemez.com">
+	<title>About - Vocational Courses</title>
+	<link rel="favicon" href="dist/images/favicon.png">
+	<link rel="stylesheet" media="screen" href="http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+	<link rel="stylesheet" href="dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="dist/css/font-awesome.min.css">
+	<!-- Custom styles for our template -->
+	<link rel="stylesheet" href="dist/css/bootstrap-theme.css" media="screen">
+	<link rel="stylesheet" href="dist/css/style.css">
+	<!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
+	<script src="assets/js/html5shiv.js"></script>
+	<script src="assets/js/respond.min.js"></script>
+	<![endif]-->
+</head>
+
+<body>
+	<!-- Fixed navbar -->
+	<div class="navbar navbar-inverse">
+		<div class="container">
+			<div class="navbar-header">
+				<!-- Button for smallest screens -->
+				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+				<a class="navbar-brand" href="index.html">
+					<img src="dist/images/logo - copy.png" alt="Techro HTML5 template"></a>
+			</div>
+			<div class="navbar-collapse collapse">
+				<ul class="nav navbar-nav pull-right mainNav">
+					<li><a href="/">Home</a></li>
+					<li><a href="{{route('about.show')}}">About</a></li>
+						<li class="active"><a href="{{route('courses.show')}}">Courses</a></li>
+					<li><a href="{{route('events.show')}}">Events</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown">Gallery<b class="caret"></b></a>
+						<ul class="dropdown-menu">
+                            <li><a href="{{route('events.show')}}">Activities</a></li>
+                            <li><a href="{{route('photos.show')}}">Photos</a></li>
+                            <li><a href="{{route('videos.show')}}">Videos</a></li>
+						</ul>
+					</li>
+					<li><a href="{{route('contact.add')}}">Contact</a></li>
+				</ul>
+			</div>
+			<!--/.nav-collapse -->
+		</div>
+	</div>
+	<!-- /.navbar -->
+
+		<header id="head" class="secondary">
+            <div class="container">
+                    <h1>Courses</h1>
+                    <p>Our vocational courses are open to the public!</p>
+                </div>
+    </header>
+
+    
+    <div class="container">
+<h3>Courses</h3>
+<p>
+    Apart from our acamedic classes we have the vocational classes to help prepared our students for the job market.
+</p>
+<br/>
+		<section class="news-box top-margin">
+        	<div class="container">
+            	<h3><span>Our Vocational Courses</span></h3>
+            	<div class="row">
+					<div class="col-lg-3 col-md-4 col-sm-4">
+                    	<div class="newsBox">
+                        	<div class="thumbnail">
+                            	<figure><img src="dist/images/images (3).jpg"" alt=""></figure>
+                            	<div class="caption maxheight2">
+                            		<div class="box_inner">
+                                        <div class="box">
+                                            <p class="title"><h5>Computer</h5></p>
+                                            <p>Lorem ipsum dolor sit amet, conc tetu er adipi scing. Praesent ves tibuum molestie lacuiirhs. Aenean.</p>
+                                        </div> 
+                                	</div>
+                            	</div>
+                        	</div>
+                   	 	</div>
+                   	</div>
+                    <div class="col-lg-3 col-md-4 col-sm-4">
+                    	<div class="newsBox">
+                        	<div class="thumbnail">
+                            	<figure><img src="dist/images/download (2).jpg" alt=""></figure>
+                           		<div class="caption maxheight2">
+                            		<div class="box_inner">
+                                        <div class="box">
+                                            <p class="title"><h5>Tylering</h5></p>
+                                            <p>Lorem ipsum dolor sit amet, conc tetu er adipi scing. Praesent ves tibuum molestie lacuiirhs. Aenean.</p>
+                                        </div> 
+                                	</div>
+                            	</div>
+                        	</div>
+                    	</div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-4">
+                    	<div class="newsBox">
+                        	<div class="thumbnail">
+                            	<figure><img src="dist/images/images.jpg" alt=""></figure>
+                            	<div class="caption maxheight2">
+                            		<div class="box_inner">
+                                        <div class="box">
+                                            <p class="title"><h5>Culinary Art</h5></p>
+                                            <p>Lorem ipsum dolor sit amet, conc tetu er adipi scing. Praesent ves tibuum molestie lacuiirhs. Aenean.</p>
+                                        </div> 
+                                	</div>
+                            	</div>
+                        	</div>
+                    	</div>
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-4">
+                    	<div class="newsBox">
+                        	<div class="thumbnail">
+                            	<figure><img src="dist/images/download (3).jpg" alt=""></figure>
+                            	<div class="caption maxheight2">
+                            		<div class="box_inner">
+                                        <div class="box">
+                                            <p class="title"><h5>Hair Dressing</h5></p>
+                                            <p>Lorem ipsum dolor sit amet, conc tetu er adipi scing. Praesent ves tibuum molestie lacuiirhs. Aenean.</p>
+                                        </div> 
+                                	</div>
+                            	</div>
+                       		</div>
+                    	</div>
+					</div>
+                </div>
+            </div>
+		</section>
+	<div id="courses">
+		<section class="container">
+			<h3>Development Courses</h3>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Computer</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Business Skille</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+				<div class="col-md-4">
+					<div class="featured-box"> 
+						<div class="text">
+							<h3>Home Economics</h3>
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			</div>
+
+		</section>
+	</div>
+    
+	<!-- container -->
+	<div class="container">
+		<div class="row">
+			<!-- Article content -->
+			<!-- <section class="col-sm-12 maincontent">
+				<h3>Responsive Website</h3>
+				<p>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				</p>
+				<h3>Bootstrap</h3>
+				<p>Standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+			</section> -->
+		</div>
+	</div>
+	<!-- /container -->
+ <footer id="footer">
+ 
+		<div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology/Computer
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business Skills
+              </a>
+            </li>
+            <li><a href="#">
+               List of Home Economic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li> <a href="#">
+                Individual Plans  </a>
+            </li>
+            <li><a href="#">
+                Business Plans
+              </a>
+            </li>
+            <li><a href="#">
+                Free Trial
+              </a>
+            </li>
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="{{route('courses.show')}}">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+            	<div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Enroll now... By visiting our campous.</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kabbah Township, Montserrado Cunty, Liberia<br>
+            <i class="fa fa-phone"></i>+123 (0)886 521 451 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+			<div class="social text-center">
+				<a href="#"><i class="fa fa-twitter"></i></a>
+				<a href="#"><i class="fa fa-facebook"></i></a>
+			</div>
+
+			<div class="clear"></div>
+			<!--CLEAR FLOATS-->
+		</div>
+		<div class="footer2">
+			<div class="container">
+				<div class="row">
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="simplenav">
+								<a href="/">Home</a> | 
+								<a href="{{route('about.show')}}">About</a> |
+								<a href="{{route('courses.show')}}">Courses</a> |
+								<a href="{{route('events.show')}}">Events</a> |
+								<a href="{{route('contact.add')}}">Contact</a>
+							</p>
+						</div>
+					</div>
+
+					<div class="col-md-6 panel">
+						<div class="panel-body">
+							<p class="text-right">
+								Copyright &copy; 2019. Template by <a rel="develop">Bolex88</a>
+							</p>
+						</div>
+					</div>
+
+				</div>
+				<!-- /row of panels -->
+			</div>
+		</div>
+	</footer>
+
+
+	<!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	<script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+	<script src="dist/js/custom.js"></script>
+</body>
+</html>
