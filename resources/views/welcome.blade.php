92caaea0b052be578e3d50dcbd01738300bc1645
James Titus Wilson <jwilsonkla@gmail.com>
2019-10-12 14:16:54 +0000

    Added the welcome page

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,312 @@
+<!--
+Author: WebThemez
+Author URL: http://webthemez.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="educational-responsive-Richard-Nanah-Gaye-Acamedy">
+    <meta name="author" content="webThemez.com">
+    <title>Richard Nanah Gaye Acamedy</title>
+    <link rel="favicon" href="dist/images/favicon.png">
+    <link rel="stylesheet" media="screen" href="dist/http://fonts.googleapis.com/css?family=Open+Sans:300,400,700">
+    <link rel="stylesheet" href="dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="dist/css/font-awesome.min.css"> 
+    <link rel="stylesheet" href="dist/css/bootstrap-theme.css" media="screen"> 
+    <link rel="stylesheet" href="dist/css/style.css">
+    <link rel='stylesheet' id='camera-css'  href='dist/css/camera.css' type='text/css' media='all'> 
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/html5shiv.js"></script>
+    <script src="assets/js/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <!-- Fixed navbar -->
+    <div class="navbar navbar-inverse">
+        <div class="container">
+            <div class="navbar-header">
+                <!-- Button for smallest screens -->
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse"><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
+                <a class="navbar-brand" href="index.html">
+                    <img src="dist/images/logo - copy.png" alt="Techro HTML5 template"></a>
+            </div>
+            <div class="navbar-collapse collapse">
+                <ul class="nav navbar-nav pull-right mainNav">
+                    <li class="active"><a href="/">Home</a></li>
+                    <li><a href="{{route('about.show')}}">About</a></li>
+                    <li><a href="{{route('courses.show')}}">Courses</a></li>
+                    <li><a href="{{route('events.show')}}">Events</a></li>
+                    <li class="dropdown">
+                        <a href="{{route('gallery.show')}}" class="dropdown-toggle" data-toggle="dropdown">Gallery<b class="caret"></b></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="{{route('events.show')}}">Activities</a></li>
+                            <li><a href="{{route('photos.show')}}">Photos</a></li>
+                            <li><a href="{{route('videos.show')}}">Videos</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="{{route('contact.add')}}">Contact</a></li>
+
+                </ul>
+            </div>
+            <!--/.nav-collapse -->
+        </div>
+    </div>
+    <!-- /.navbar -->
+
+    <!-- Header -->
+    <header id="head">
+        <div class="container">
+             <div class="heading-text">                         
+                            <h1 class="animated flipInY delay1">Start Your Education With RINEGA</h1>
+                            <p>Welcome Back To School.</p>
+                        </div>
+            
+                    <div class="fluid_container">                       
+                    <div class="camera_wrap camera_emboss pattern_1" id="camera_wrap_4">
+                        <div data-thumb="dist/images/Student.jpg" data-src="dist/images/Student.jpg">
+                            <h2>We Develop.</h2>
+                        </div> 
+                        <div data-thumb="dist/images/student1.jpg" data-src="dist/images/student1.jpg">
+                        </div>
+                        <div data-thumb="dist/images/student6.jpg" data-src="dist/images/student6.jpg">
+                        </div> 
+                    </div><!-- #camera_wrap_3 -->
+                </div><!-- .fluid_container -->
+        </div>
+    </header>
+    <!-- /Header -->
+
+  <div class="container">
+    <div class="row">
+                    <div class="col-md-3">
+                        <div class="grey-box-icon">
+                            <div class="icon-box-top grey-box-icon-pos">
+                                <img src="dist/images/1.png" alt="" />
+                            </div><!--icon box top -->
+                            <h4>Vocational Courses</h4>
+                            <p>Vocational courses and classes are available in many different career fields, such as tylering, computer technology, office management and business skilled trades.</p>
+                            <p><a href="{{route('courses.show')}}"><em>Read More</em></a></p>
+                        </div><!--grey box -->
+                    </div><!--/span3-->
+                    <div class="col-md-3">
+                        <div class="grey-box-icon">
+                            <div class="icon-box-top grey-box-icon-pos">
+                                <img src="dist/images/2.png" alt="" />
+                            </div><!--icon box top -->
+                            <h4>Meet our Staff</h4>
+                            <p>At Richard Nanah Gaye Acamedy we have: <b //>
+                            A dedicated enthusiastic professional staff. An inclusive and innovative curriculum.</p>
+                            <p><a href="{{route('about.show')}}"><em>Read More</em></a></p>
+                        </div><!--grey box -->
+                    </div><!--/span3-->
+                    <div class="col-md-3">
+                        <div class="grey-box-icon">
+                            <div class="icon-box-top grey-box-icon-pos">
+                                <img src="dist/images/3.png" alt="" />
+                            </div><!--icon box top -->
+                            <h4>Latest Updates</h4>
+                            <p>Vocational courses and classes are available in many different career fields, such as tylering, computer technology, office management and business skilled trades.</p>
+                            <p><a href="{{route('events.show')}}"><em>Read More</em></a></p>
+                        </div><!--grey box -->
+                    </div><!--/span3-->
+                    <div class="col-md-3">
+                        <div class="grey-box-icon">
+                            <div class="icon-box-top grey-box-icon-pos">
+                                <img src="dist/images/4.png" alt="" />
+                            </div><!--icon box top -->
+                            <h4>Placements</h4>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent tempus eleifend risus ut congue
+                            eset nec lacus elit dor broma.</p>
+                            <p><a href="#"><em>Read More →</em></a></p>
+                        </div><!--grey box -->
+                    </div><!--/span3-->
+                </div>
+    </div> <br />
+  
+    
+      <section class="container">
+      <div class="row">
+        <div class="col-md-8"><div class="title-box clearfix "><h2 class="title-box_primary">About Us</h2></div> 
+        <p><span>At Richard Nanah Gaye Acamedy our "Learning has no Limits", and this is firmly embedded in our learning community.</span></p>
+        <p>Our focus is to support and develop enthusiastic students, and instal in them a love of learning so as to help them reach their potential, both socially and academically. As a learning community, we pride ourselves in the development of the whole child, values and social skills are an importan part of the school curriumlum and we are focused on providing an inclusive education for all.</p><p>We are committed to providing a high qualityteaching and learning program that gives all of our students the opportunity to achieve success. With high expectations, we work together with the wider school community to prepare our students to become respected and contributing members of society.</p>
+        <a href="{{route('about.show')}}" title="read more" class="btn-inline " target="_self">read more</a> </div> <br />
+              
+          
+          <div class="col-md-4"><div class="title-box clearfix "><h2 class="title-box_primary">Up Coming Events</h2></div> 
+            <div class="list styled custom-list">
+            <ul>
+            <li><a title="Snatoque penatibus et magnis dis partu rient montes ascetur ridiculus mus." href="#">Computer Science</a></li>
+            <li><a title="Fusce feugiat malesuada odio. Morbi nunc odio gravida at cursus nec luctus." href="#">Business Skills</a></li>
+            <li><a title="Penatibus et magnis dis parturient montes ascetur ridiculus mus." href="#">Home Economic</a></li>
+            <li><a title="Penatibus et magnis dis parturient montes ascetur ridiculus mus." href="#">Office Management</a></li>
+            </ul>
+            </div>
+         </div>
+      </div>
+      </section>
+      
+         
+    <footer id="footer">
+ 
+        <div class="container">
+   <div class="row">
+  <div class="footerbottom">
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Course Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                List of Technology/Computer
+              </a>
+            </li>
+            <li><a href="#">
+                List of Business Skills
+              </a>
+            </li>
+            <li><a href="#">
+               List of Home Economic
+              </a>
+            </li>
+            <li><a href="#">
+               List of Office Management
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Products Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="#">
+                Academic
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6">
+      <div class="footerwidget">
+        <h4>
+          Browse by Categories
+        </h4>
+        <div class="menu-course">
+          <ul class="menu">
+            <li><a href="{{route('courses.show')}}">
+                All Courses
+              </a>
+            </li>
+            <li> <a href="#">
+                All Instructors
+              </a>
+            </li>
+            <li><a href="#">
+                All Members
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                All Groups
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="col-md-3 col-sm-6"> 
+                <div class="footerwidget"> 
+                         <h4>Contact</h4> 
+                        <p>Enroll now... By visiting our campous.</p>
+            <div class="contact-info"> 
+            <i class="fa fa-map-marker"></i> Kabbah Township, Montserrado Cunty, Liberia<br>
+            <i class="fa fa-phone"></i>+123 (0)886 521 451 <br>
+             <i class="fa fa-envelope-o"></i> youremail@email.com
+              </div> 
+                </div><!-- end widget --> 
+    </div>
+  </div>
+</div>
+            <div class="social text-center">
+                <a href="#"><i class="fa fa-twitter"></i></a>
+                <a href="#"><i class="fa fa-facebook"></i></a>
+            </div>
+
+            <div class="clear"></div>
+            <!--CLEAR FLOATS-->
+        </div>
+        <div class="footer2">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="simplenav">
+                                <a href="/">Home</a> | 
+                                <a href="{{route('about.show')}}">About</a> |
+                                <a href="{{route('courses.show')}}">Courses</a> |
+                                <a href="{{route('events.show')}}">Events</a> |
+                                <a href="{{route('contact.add')}}">Contact</a>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div class="col-md-6 panel">
+                        <div class="panel-body">
+                            <p class="text-right">
+                                Copyright &copy; 2019. Template by <a href="#" rel="develop">Bolex88</a>
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- /row of panels -->
+            </div>
+        </div>
+    </footer>
+
+    <!-- JavaScript libs are placed at the end of the document so the pages load faster -->
+    <script src="dist/js/modernizr-latest.js"></script> 
+    <script type='text/javascript' src='dist/js/jquery.min.js'></script>
+    <script type='text/javascript' src='dist/js/fancybox/jquery.fancybox.pack.js'></script>
+    
+    <script type='text/javascript' src='dist/js/jquery.mobile.customized.min.js'></script>
+    <script type='text/javascript' src='dist/js/jquery.easing.1.3.js'></script> 
+    <script type='text/javascript' src='dist/js/camera.min.js'></script> 
+    <script src="dist/js/bootstrap.min.js"></script> 
+    <script src="dist/js/custom.js"></script>
+    <script>
+        jQuery(function(){
+            
+            jQuery('#camera_wrap_4').camera({
+                transPeriod: 500,
+                time: 3000,
+                height: '600',
+                loader: 'false',
+                pagination: true,
+                thumbnails: false,
+                hover: false,
+                playPause: false,
+                navigation: false,
+                opacityOnGrid: false,
+                imagePath: 'assets/images/'
+            });
+
+        });
+      
+    </script>
+    
+</body>
+</html>
